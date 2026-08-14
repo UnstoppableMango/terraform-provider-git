dee244182c8bc834952afdf0a4c2c33f48513460
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-08-13 20:58:56 -0500

    git repo (#2)
    * git_repository schema
    
    * Ignore claude
    
    * Mod update

--- main.go
+++ main.go
@@ -4,42 +4,13 @@ import (
 	"context"
 	"log"
 
-	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/provider"
-	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
+
+	"github.com/UnstoppableMango/terraform-provider-git/internal/provider"
 )
 
-type gitProvider struct{}
-
-var _ provider.Provider = (*gitProvider)(nil)
-
-func New() provider.Provider {
-	return &gitProvider{}
-}
-
-func (p *gitProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "git"
-}
-
-func (p *gitProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{}
-}
-
-func (p *gitProvider) Configure(_ context.Context, _ provider.ConfigureRequest, _ *provider.ConfigureResponse) {
-}
-
-func (p *gitProvider) DataSources(_ context.Context) []func() datasource.DataSource {
-	return nil
-}
-
-func (p *gitProvider) Resources(_ context.Context) []func() resource.Resource {
-	return nil
-}
-
 func main() {
-	err := providerserver.Serve(context.Background(), New, providerserver.ServeOpts{
+	err := providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/UnstoppableMango/git",
 	})
 	if err != nil {
